4467d000389249df3576f873dd4c0fa74c055603
bruceLee <K1131198679@163.com>
2020-12-17 14:32:22 +0800

    feat: add per page

--- app/web/user.py
+++ app/web/user.py
@@ -4,9 +4,10 @@
 # @Author  : BruceLee
 # @Site    : 
 # @File    : user.py
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 
 from app.spider.user_source import User
+from ..spider.page import Pagination
 from ..web import web
 
 
@@ -80,3 +81,17 @@ def bind_user_family_relation():
         return jsonify(trace)
     else:
         return {"msg": "bind user family relation"}
+
+
+@web.route("/pager")
+def pager():
+    li = []
+    for i in range(1, 100):
+        li.append(i)
+    pager_obj = Pagination(request.args.get("page", 1), len(li), request.path, request.args, per_page_count=10)
+    # print(request.args)
+    index_list = li[pager_obj.start:pager_obj.end]
+    html = pager_obj.page_html()
+    return render_template("index.html", index_list=index_list, html=html)
+
+
